afafb0ef7067cce115cf2ec9b48e6ff1e8ff2ba3
CornWorld <cw@had.ink>
2023-03-19 18:17:48 +0800

    [Change] Split `DataOperator`
    Also fix an type error.

--- packages/server/GameJudge/internal/DataOperator/get.go
+++ packages/server/GameJudge/internal/DataOperator/get.go
@@ -3,40 +3,39 @@ package DataOperator
 import (
 	"encoding/json"
 	"server/ApiProvider/pkg/InstructionType"
+	"server/GameJudge/internal/DataOperator/local"
+	"server/GameJudge/internal/GameType"
 	"server/MapHandler/pkg/MapType"
 	_ "server/MapHandler/pkg/MapType"
 )
 
-const exampleMap string = "[\n[0,0,0,0,2],\n[0,2,0,0,0],\n[0,0,0,0,0],\n[0,3,3,0,3],\n[0,3,0,2,0]\n]"
-
-var exampleInstruction = []InstructionType.Instruction{
-	InstructionType.MoveInstruction{UserId: 1, Position: MapType.BlockPosition{X: 1, Y: 1}, Towards: InstructionType.MoveTowardsDown},
-}
-
 func GetOriginalGameMap(mapId uint32) MapType.Map {
-	var originMapStr string
-	if mapId == 0 {
-		originMapStr = exampleMap
-	} else {
-		// TODO: Get from the db
-	}
+	originMapStr := local.GetOriginalGameMapStr(mapId)
 	var result [][]uint8
 	if json.Unmarshal([]byte(originMapStr), &result) == nil {
 		// TODO: Error return
 	}
-
 	ret := make([][]MapType.Block, len(result))
 	for rowNum, row := range result {
 		ret[rowNum] = make([]MapType.Block, len(row))
 		for colNum, typeId := range row {
-			ret[rowNum][colNum] = MapType.ToBlockByTypeId(typeId, MapType.BaseBlock{})
-
+			ret[rowNum][colNum] = MapType.ToBlockByTypeId(typeId, &MapType.BaseBlock{})
 		}
 	}
 	return MapType.Map{Blocks: ret, Size: MapType.MapSize{X: uint8(len(ret[0])), Y: uint8(len(ret))}, MapId: mapId}
 }
 
-func GetInstruction() []InstructionType.Instruction {
+var MapTemp map[GameType.GameId]*MapType.Map
+
+func init() {
+	MapTemp = make(map[GameType.GameId]*MapType.Map)
+}
+
+func GetCurrentMap(id GameType.GameId) *MapType.Map {
+	return MapTemp[id]
+}
+
+func GetInstruction(id GameType.GameId) []InstructionType.Instruction {
 	// TODO: Stop receiving new instruction
-	return exampleInstruction
+	return local.ExampleInstruction
 }
---
+++ packages/server/GameJudge/internal/DataOperator/local/local.go
@@ -0,0 +1,33 @@
+package local
+
+import (
+	"server/ApiProvider/pkg/InstructionType"
+	"server/GameJudge/internal/GameType"
+	"server/MapHandler/pkg/MapType"
+)
+
+var originalMap map[uint32]string
+var GamePool map[uint16]*GameType.Game
+var UserList map[uint16][]GameType.User
+
+func init() {
+	originalMap = make(map[uint32]string)
+	originalMap[0] = "[\n[0,0,0,0,2],\n[0,2,0,0,0],\n[0,0,0,0,0],\n[0,3,3,0,3],\n[0,3,0,2,0]\n]"
+
+	GamePool = make(map[uint16]*GameType.Game)
+
+	UserList = make(map[uint16][]GameType.User)
+	UserList[0] = []GameType.User{
+		{"Tester1", 1, GameType.UserStatusConnected},
+		{"Tester2", 2, GameType.UserStatusConnected},
+	}
+}
+
+func GetOriginalGameMapStr(mapId uint32) string {
+	return originalMap[mapId]
+}
+
+var ExampleInstruction = []InstructionType.Instruction{
+	InstructionType.MoveInstruction{UserId: 1, Position: MapType.BlockPosition{X: 1, Y: 1}, Towards: InstructionType.MoveTowardsDown},
+	InstructionType.MoveInstruction{UserId: 2, Position: MapType.BlockPosition{X: 1, Y: 1}, Towards: InstructionType.MoveTowardsDown},
+}
