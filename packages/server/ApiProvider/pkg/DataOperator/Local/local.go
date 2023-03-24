7b0e9bdf6ef7de8b44288f1562fdb78cec4d1931
CornWorld <cw@had.ink>
2023-03-24 12:47:37 +0800

    [Feat] Complete local data source

--- packages/server/ApiProvider/pkg/DataOperator/Local/local.go
+++ packages/server/ApiProvider/pkg/DataOperator/Local/local.go
@@ -12,9 +12,10 @@ import (
 var _ DataOperator.DataSource = (*Local)(nil)
 
 type Local struct {
-	GamePool           map[uint16]*GameType.Game
-	OriginalMapStrPool map[uint32]string
-	InstructionPool    map[uint16][]InstructionType.Instruction
+	GamePool            map[uint16]*GameType.Game
+	OriginalMapStrPool  map[uint32]string
+	InstructionTempPool map[uint16][]InstructionType.Instruction
+	InstructionLog      map[uint16]map[uint8][]InstructionType.Instruction
 }
 
 var Pool Local
@@ -24,6 +25,8 @@ func init() {
 	Pool.OriginalMapStrPool = make(map[uint32]string)
 	Pool.OriginalMapStrPool[0] = "[\n[0,0,0,0,2],\n[0,2,0,0,0],\n[0,0,0,0,0],\n[0,3,3,0,3],\n[0,3,0,2,0]\n]"
 
+	Pool.InstructionTempPool = make(map[uint16][]InstructionType.Instruction)
+	Pool.InstructionLog = make(map[uint16]map[uint8][]InstructionType.Instruction)
 }
 
 func (l *Local) GetOriginalMap(mapId uint32) *MapType.Map {
@@ -45,12 +48,7 @@ func (l *Local) CreateGame(game *GameType.Game) GameType.GameId {
 }
 
 func (l *Local) PutInstructions(id GameType.GameId, instructions []InstructionType.Instruction) bool {
-	l.InstructionPool[uint16(id)] = instructions
-	return true
-}
-
-func (l *Local) AnnounceGameStart(gameId GameType.GameId) bool {
-	// TODO
+	l.InstructionTempPool[uint16(id)] = instructions
 	return true
 }
 
@@ -59,13 +57,22 @@ func (l *Local) AnnounceGameStart(gameId GameType.GameId) bool {
 //	InstructionType.MoveInstruction{UserId: 2, Position: MapType.BlockPosition{X: 1, Y: 1}, Towards: InstructionType.MoveTowardsDown},
 //}
 
-func (l *Local) GetCurrentInstruction(id GameType.GameId) []InstructionType.Instruction {
-	return l.InstructionPool[uint16(id)]
+func (l *Local) AnnounceGameStart(gameId GameType.GameId) bool {
+	//TODO implement me
+	panic("implement me")
 }
 
-func (l *Local) NewRound(id GameType.GameId, m *MapType.Map, roundNum uint8) bool {
-	g := l.GamePool[uint16(id)]
-	g.Map = m
-	g.RoundNum = roundNum
+func (l *Local) GetInstructionsFromTemp(id GameType.GameId, roundNum uint8) []InstructionType.Instruction {
+	return l.InstructionLog[uint16(id)][roundNum]
+}
+
+func (l *Local) AchieveInstructionTemp(id GameType.GameId, roundNum uint8) bool {
+	l.InstructionLog[uint16(id)][roundNum] = l.InstructionTempPool[uint16(id)]
+	l.InstructionTempPool[uint16(id)] = []InstructionType.Instruction{}
+	return true
+}
+
+func (l *Local) PutMap(id GameType.GameId, m *MapType.Map) bool {
+	l.GamePool[uint16(id)].Map = m
 	return true
 }
