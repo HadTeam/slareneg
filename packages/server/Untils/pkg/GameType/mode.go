8dc1a23fba96718572941259d7682e38c86900d1
CornWorld <cw@had.ink>
2023-04-11 19:32:49 +0800

    [Feat] Add a mark string in `GameMode` struct

--- packages/server/Untils/pkg/GameType/mode.go
+++ packages/server/Untils/pkg/GameType/mode.go
@@ -3,6 +3,7 @@ package GameType
 type GameMode struct {
 	MaxUserNum uint8
 	MinUserNum uint8
+	NameStr    string
 }
 
-var GameMode1v1 = GameMode{MaxUserNum: 2, MinUserNum: 2}
+var GameMode1v1 = GameMode{MaxUserNum: 2, MinUserNum: 2, NameStr: "1v1"}
