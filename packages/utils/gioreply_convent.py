7ce23718653012a7b4bb3beb697b44736661d7b7
CornWorld <cw@had.ink>
2023-04-16 00:46:51 +0800

    [Fix] Add adaptation of the newest command format

--- packages/utils/gioreply_convent.py
+++ packages/utils/gioreply_convent.py
@@ -25,16 +25,16 @@ with open(sys.argv[2], 'w') as f:
         after_location = [move['end'] % w, int(move['end'] / w)]
         if prev_location[0] > after_location[0]:
             toward = 'left'
-            distance = prev_location[0] - after_location[0]
         elif prev_location[0] < after_location[0]:
             toward = 'right'
-            distance = after_location[0] - prev_location[0]
         elif prev_location[1] > after_location[1]:
             toward = 'up'
-            distance = prev_location[1] - after_location[1]
         elif prev_location[1] < after_location[1]:
             toward = 'down'
-            distance = after_location[1] - prev_location[1]
         else:
             raise Exception('Error')
-        f.write(f'Move {prev_location[0]} {prev_location[1]} {toward} {distance}\n')
+        if int(move['is50']):
+            number = 65535
+        else:
+            number = 0
+        f.write(f'Move {prev_location[0]} {prev_location[1]} {toward} {number}\n')
