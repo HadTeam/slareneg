7ed8c77db9c05788f921ce07cae9902a84c9928d
CornWorld <cw@had.ink>
2023-05-11 13:18:03 +0800

    [Feat] Complete file reciver

--- packages/server/ApiProvider/internal/Receiver/file.go
+++ packages/server/ApiProvider/internal/Receiver/file.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"server/ApiProvider/internal/CommandPauser"
+	"server/JudgePool"
 	"server/Utils/pkg/DataSource"
 	"server/Utils/pkg/GameType"
 	"server/Utils/pkg/MapType"
@@ -23,29 +24,22 @@ func ApplyDataSource(source any) {
 	data = source.(DataSource.TempDataSource)
 }
 
-func NewFileReceiver() {
+func NewFileReceiver(pool *JudgePool.Pool) {
 	f := LoadFile()
 
-	selectWaitingGame := func() []GameType.Game {
-		var gl []GameType.Game
-		for {
-			if gl = data.GetGameList(GameType.GameMode1v1); gl != nil {
-				break
-			}
-		}
-		var ret []GameType.Game
-		for _, g := range gl {
-			if g.Status == GameType.GameStatusWaiting {
-				ret = append(ret, g)
-			}
-		}
-		return ret
-	}
-	for _, r := range f {
+	for index, r := range f {
 		time.Sleep(time.Millisecond * 200)
-		fmt.Printf("Start game by reply file, index %d\n", r.Id)
-		g := selectWaitingGame()[0]
-
+		log.Printf("Start game by reply file, index %d\n", r.Id)
+		g := &GameType.Game{
+			Map:        r.Map,
+			Mode:       GameType.GameMode1v1,
+			Id:         GameType.GameId(index + 1e3),
+			UserList:   []GameType.User{},
+			CreateTime: time.Now().UnixMicro(),
+			Status:     GameType.GameStatusWaiting,
+			RoundNum:   0,
+		}
+		pool.DebugNewGame(g)
 		for _, c := range r.UserPack {
 			ctx := Context{
 				Context: context.TODO(),
@@ -87,7 +81,7 @@ func LoadFile() []reply {
 		if len(s) != 3 {
 			continue
 		}
-		if s[1] != "gioreplay" || s[2] != "processed" {
+		if s[1] != "gioreplay" || s[2] != "processed" { // allows *.gioreplay.processed files
 			continue
 		}
 
@@ -105,7 +99,7 @@ func LoadFile() []reply {
 		part := strings.Split(string(fileBuf), "|")
 
 		m := part[0]
-		r.Map = pkg.FullStr2GameMap(uint32(index), m)
+		r.Map = MapType.FullStr2GameMap(uint32(index), m)
 
 		for userId, p := range part { // set the index of user part as user id
 			if userId == 0 {
@@ -119,12 +113,12 @@ func LoadFile() []reply {
 			cmd := command{
 				User: GameType.User{
 					Name:             name,
-					UserId:           uint16(userId) - 1,
+					UserId:           uint16(userId),
 					Status:           GameType.UserStatusConnected,
 					TeamId:           uint8(userId) - 1,
 					ForceStartStatus: false,
 				},
-				Ins: append([]string{""}, cmdStr...),
+				Ins: append([]string{""}, cmdStr...), // add an empty string to skip round 0
 			}
 
 			r.UserPack = append(r.UserPack, cmd)
@@ -136,35 +130,41 @@ func LoadFile() []reply {
 }
 
 func fakePlayer(ctx *Context, c []string) {
+	// DO NOT MODIFY `ctx`(except channel sending) 'cause it is read-only for a real player
 	ticker := time.NewTicker(50 * time.Millisecond)
 	currentRound := uint16(0)
 	for {
 		select {
 		case <-ticker.C:
 			{
-				ctx.Game = data.GetGameInfo(ctx.Game.Id) // TODO
+				g := data.GetGameInfo(ctx.Game.Id)
 				if currentRound >= uint16(len(c)) {
+					log.Printf("[Game %d] Fake Player %s Command(tot: %d) runs out, quit\n", ctx.Game.Id, ctx.User.Name, len(c))
 					ticker.Stop()
 				}
 				if ctx.Game.RoundNum > currentRound {
-					currentRound = ctx.Game.RoundNum
-					ctx.Command <- c[currentRound]
+					log.Printf("[Game %d] Fake Player %s Discover new round %d\n", ctx.Game.Id, ctx.User.Name, g.RoundNum)
+					currentRound = g.RoundNum
+					if currentRound < uint16(len(c)) {
+						log.Printf("[Game %d] Fake Player %s Send command '%s'\n", ctx.Game.Id, ctx.User.Name, c[currentRound])
+						ctx.Command <- c[currentRound]
+					}
 				}
 			}
 		case msg := <-ctx.Message:
 			{
-				log.Printf("Game %d User %s Msg: %s\n", ctx.Game.Id, ctx.User.Name, msg[:19])
+				log.Printf("[Game %d] Fake Player %s Msg: %s\n", ctx.Game.Id, ctx.User.Name, msg[:30]) // output a part of message avoiding excessive output
 			}
 		}
 	}
 }
 
 func receiver(ctx *Context) {
-	ctx.User.Name = strconv.Itoa(int(ctx.User.UserId)) // TODO DEBUG ONLY
+	ctx.User.Name = strconv.Itoa(int(ctx.User.UserId)) // DEBUG ONLY, avoiding strange username from `gioreply` file
 	ctx.User.Status = GameType.UserStatusConnected
 	data.SetUserStatus(ctx.Game.Id, ctx.User)
 
-	log.Printf("Game %d User %s join\n", ctx.Game.Id, ctx.User.Name)
+	log.Printf("[Game %d] User %s join\n", ctx.Game.Id, ctx.User.Name)
 
 	defer func() {
 		ctx.User.Status = GameType.UserStatusDisconnected
@@ -174,11 +174,11 @@ func receiver(ctx *Context) {
 	data.SetUserStatus(ctx.Game.Id, ctx.User)
 
 	ticker := time.NewTicker(50 * time.Millisecond)
-	for i := 1; true; i++ {
+	flag := true
+	for i := 1; flag; i++ {
 		select {
 		case <-ctx.Done():
 			{
-				log.Println("ctx done")
 				return
 			}
 		case cmd := <-ctx.Command:
@@ -202,21 +202,16 @@ func receiver(ctx *Context) {
 
 				// Check game status
 				g := data.GetGameInfo(ctx.Game.Id)
-
-				//if i%100 == 0 {
-				//	log.Println(ctx.User.UserId, i, ctx.Game.RoundNum, g.RoundNum)
-				//}
-				// TODO
-
 				if g.Status != ctx.Game.Status {
 					if ctx.Game.Status == GameType.GameStatusWaiting && g.Status == GameType.GameStatusRunning {
 						done(g, "info")
 						done(g, "start")
 						continue
-					} else if ctx.Game.Status == GameType.GameStatusRunning && g.Status == GameType.GameStatusEnd {
+					} else if g.Status == GameType.GameStatusEnd {
 						done(g, "end")
 						ticker.Stop()
-						continue
+						flag = false
+						break
 					}
 				} else {
 					if i%20 == 0 {
