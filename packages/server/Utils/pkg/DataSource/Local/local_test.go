a68bf17972046d43cae2831e0e79d48c9dafd698
CornWorld <cw@had.ink>
2023-05-21 06:25:08 +0800

    [Clean] Remove debug code and refine variable name

--- packages/server/Utils/pkg/DataSource/Local/local_test.go
+++ packages/server/Utils/pkg/DataSource/Local/local_test.go
@@ -50,12 +50,13 @@ func TestLocal_lock(t *testing.T) {
 
 func TestLocal_Game(t *testing.T) {
 	l := create()
+	id := GameType.GameId(1)
 
 	// This function must be correct
 	l.DebugCreateGame(&GameType.Game{
 		Map:        MapType.FullStr2GameMap(1, "[[[0,0,0]]]"), // Wait
 		Mode:       GameType.GameMode1v1,
-		Id:         1,
+		Id:         id,
 		UserList:   []GameType.User{getUser()},
 		CreateTime: 0,
 		Status:     GameType.GameStatusWaiting,
@@ -64,14 +65,14 @@ func TestLocal_Game(t *testing.T) {
 	})
 
 	t.Run("set game status", func(t *testing.T) {
-		l.SetGameStatus(1, GameType.GameStatusRunning)
-		if l.GamePool[1].Status != GameType.GameStatusRunning {
+		l.SetGameStatus(id, GameType.GameStatusRunning)
+		if l.GamePool[id].Status != GameType.GameStatusRunning {
 			t.Fatalf("the status has unchanged")
 		}
 	})
 	t.Run("get game status", func(t *testing.T) {
-		g := l.GetGameInfo(1)
-		if g == l.GamePool[1] {
+		g := l.GetGameInfo(id)
+		if g == l.GamePool[id] {
 			t.Fatalf("game struct copied")
 		}
 	})
@@ -80,36 +81,36 @@ func TestLocal_Game(t *testing.T) {
 		if len(gl) != 1 {
 			t.Fatalf("game count is incorrect")
 		}
-		if gl[0].Id != 1 {
+		if gl[0].Id != id {
 			t.Fatalf("game info is incorrect")
 		}
 	})
 
 	t.Run("instruction temp", func(t *testing.T) {
-		l.NewInstructionTemp(1, 1)
-		if l.InstructionLog[1][1] == nil {
+		l.NewInstructionTemp(id, 1)
+		if l.InstructionLog[id][1] == nil {
 			t.Fatalf("instruction temp has not been created")
 		}
 	})
 	t.Run("update instruction", func(t *testing.T) {
-		u := l.GamePool[1].UserList[0]
+		u := l.GamePool[id].UserList[0]
 		ins := InstructionType.Move{
 			Position: InstructionType.BlockPosition{1, 1},
 			Towards:  "down",
 			Number:   1,
 		}
-		l.UpdateInstruction(1, u, ins)
-		if l.InstructionLog[1][1][u.UserId] != ins {
+		l.UpdateInstruction(id, u, ins)
+		if l.InstructionLog[id][1][u.UserId] != ins {
 			t.Fatalf("instruction temp havn't created")
 		}
 	})
 
 	t.Run("cancel game", func(t *testing.T) {
-		l.CancelGame(1)
-		if l.GamePool[1].Status != GameType.GameStatusEnd {
+		l.CancelGame(id)
+		if l.GamePool[id].Status != GameType.GameStatusEnd {
 			t.Fatalf("the status has unchanged")
 		}
-		if l.GamePool[1].UserList != nil {
+		if l.GamePool[id].UserList != nil {
 			t.Fatalf("the userlist has not been cleared")
 		}
 	})
@@ -117,10 +118,11 @@ func TestLocal_Game(t *testing.T) {
 
 func TestLocal_User(t *testing.T) {
 	l := create()
+	id := GameType.GameId(2)
 	l.DebugCreateGame(&GameType.Game{
 		Map:        MapType.FullStr2GameMap(1, "[[[0,0,0]]]"),
 		Mode:       GameType.GameMode1v1,
-		Id:         2,
+		Id:         id,
 		UserList:   []GameType.User{},
 		CreateTime: 0,
 		Status:     GameType.GameStatusWaiting,
@@ -129,27 +131,22 @@ func TestLocal_User(t *testing.T) {
 	})
 	u := getUser()
 	t.Run("set user status", func(t *testing.T) {
-		l.SetUserStatus(2, u)
-		if l.GamePool[2].UserList != nil && len(l.GamePool[2].UserList) != 0 {
+		l.SetUserStatus(id, u)
+		if l.GamePool[id].UserList != nil && len(l.GamePool[id].UserList) != 0 {
 			t.Fatalf("user has been added unexpectedly")
 		}
 
 		u.Status = GameType.UserStatusConnected
-		l.SetUserStatus(2, u)
+		l.SetUserStatus(id, u)
 
-		if len(l.GamePool[2].UserList) != 1 || l.GamePool[2].UserList[0].UserId != u.UserId {
+		if len(l.GamePool[id].UserList) != 1 || l.GamePool[id].UserList[0].UserId != u.UserId {
 			t.Fatalf("user has not been added")
 		}
 
 		u.Status = GameType.UserStatusDisconnected
 
-		t.Log(l.GamePool[2].UserList)
-		l.SetUserStatus(2, u)
+		l.SetUserStatus(id, u)
 
-		t.Log(l.GamePool[2].UserList)
-		//l.GamePool[2].UserList = append(l.GamePool[2].UserList[:0], l.GamePool[2].UserList[0+1:]...) // TODO ?
-		//
-		//t.Log(l.GamePool[2].UserList)
 		if len(l.GamePool[2].UserList) != 0 {
 			t.Fatalf("user has not been removed")
 		}
